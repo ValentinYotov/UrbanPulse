6e54f0962ddb551faa9c3f1ea752a7fd8cd8b485
Valentin <vyotov07@gmail.com>
2025-06-01 19:52:05 +0300

    Remove firebase-debug.log file and add a blank line to firebase-config.js for improved readability.

--- UrbanPulse/firebase-config.js
+++ UrbanPulse/firebase-config.js
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
